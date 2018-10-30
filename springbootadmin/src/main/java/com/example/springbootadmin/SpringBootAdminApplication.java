69508fdfd6ebe9bd2ea32ed3588ce43c75bf5b56
panchalhitesh <hiteshnpanchal76@gmail.com>
2018-10-29 23:20:04 -0400

    updated to Spring Boot App
    Updated Spring Boot App with Hystrix with turbine Dashboards

--- springbootadmin/src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
+++ springbootadmin/src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +16,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableTurbine
 public class SpringBootAdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminApplication.class, args);
