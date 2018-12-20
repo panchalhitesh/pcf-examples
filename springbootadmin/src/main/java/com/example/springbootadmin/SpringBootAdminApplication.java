949706a4eda64e562077aeeddd02e8835d244c12
panchalhitesh <panchalhitesh@users.noreply.github.com>
2018-12-20 16:42:15 -0500

    kubernetes discovery scheduling

--- springbootadmin/src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
+++ springbootadmin/src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
@@ -9,12 +9,14 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
+mport org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@EnableScheduling
 @EnableAdminServer
 @EnableHystrix
 @EnableHystrixDashboard
